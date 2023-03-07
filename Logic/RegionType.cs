47bbbcf30ba23e30a4729ad01b1d1e6406339b6e
Carlos Ulate Herrera <humberto93cr@hotmail.com>
2023-03-06 21:12:51 -0600

    Se agrega funcionamiento de Unit test

---
+++ Logic/RegionType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class RegionType
+    {
+        Model.RegionType model = new Model.RegionType();
+        
+        public RegionType() 
+        {
+            model.IdRegion = 0;
+            model.IdCapacityType = 0;
+            model.Cost = 0;
+        }
+
+        public RegionType(Model.RegionType model)
+        {
+            this.model = model;
+        }
+
+       public bool Create(string cnn)
+       {
+           Database.RegionType region = new Database.RegionType();
+           return region.Create(model, cnn);
+       }
+
+        public bool Update(string cnn)
+        {
+            Database.RegionType region = new Database.RegionType();
+            return region.Update(model, cnn);
+        }
+
+        public bool Delete(string cnn)
+        {
+            Database.RegionType region = new Database.RegionType();
+            return region.Delete(model, cnn);
+        }
+
+        public List<Model.RegionType> GetRegions(string cnn, string where = "")
+        {
+            Database.RegionType region = new Database.RegionType();
+            return region.Get(cnn, where);
+        }
+    }
+}
---
+++ LogicTests/Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Tests
+{
+    [TestClass()]
+    public class Test
+    {
+        [TestMethod()]
+        public void calcularCosto()
+        {
+            Logic.Test calc = new Logic.Test();
+
+            Assert.AreEqual(calc.calcularCosto(10, 1).Count(), 0);
+        }
+    }
+}
